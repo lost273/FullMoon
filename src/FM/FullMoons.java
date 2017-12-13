20fff5f74ea8a231a3a519dc721ed5e81e5ad9b5
lost273 <dnk273@gmail.com>
2017-12-13 13:33:53 +0300

    13-12-2017

--- src/FM/FullMoons.java
+++ src/FM/FullMoons.java
@@ -2,22 +2,22 @@
 package FM;
 
 import java.util.*;
-import java.io.*;
+
 import static java.lang.System.out;
 
 public class FullMoons {
     static int DAY_IM = 1000 * 60 * 60 *24;
     public static void main(String[] args){
         Calendar c = Calendar.getInstance();
-        c.set(2004,1,7,15,40);
+        c.set(2004,0,7,15,40);
         long day1 = c.getTimeInMillis();
         
         
         
         for (int x = 0; x < 60; x++){
-            out.println(String.format("Full moon on %tc", c));
             day1 += (DAY_IM * 29.52);
             c.setTimeInMillis(day1);
+            out.println(String.format("Full moon on %tc", c));
         }
     }
 }
